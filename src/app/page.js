436f19c8e845f8b10a41e1ccc13c7b5225f1d38f
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-06-28 00:57:32 +0600

    Add required validation to form by react-hook-form tool

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,10 @@ import { useForm } from "react-hook-form";
 
 
 export default function Home() {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, formState: { errors }, } = useForm()
 
   const onSubmit = data => {
     console.log(data);
-
   }
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -20,41 +19,57 @@ export default function Home() {
         <div>
           <label className="block mb-2 font-medium">Name</label>
           <input
-            {...register('name')}
+            {...register("name", { required: true })}
+            aria-invalid={errors.name ? "true" : "false"}
             type="text"
             placeholder="Enter your name"
             className="w-full px-4 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-blue-500"
           />
+          {errors.name?.type === "required" && (
+            <p role="alert" className="text-red-500">- Name is required*</p>
+          )}
         </div>
 
         <div>
           <label className="block mb-2 font-medium">Email</label>
           <input
-            {...register('email')}
+            {...register('email', { required: true })}
+            aria-invalid={errors.email ? 'true' : 'false'}
             type="email"
             placeholder="Enter your email"
             className="w-full px-4 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-blue-500"
           />
+          {errors.email?.type === 'required' && (
+            <p role="alert" className="text-red-500"> - Email is required*</p>
+          )}
         </div>
 
         <div>
           <label className="block mb-2 font-medium">Password</label>
           <input
-            {...register('password')}
+            {...register('password', { required: true })}
+            aria-invalid={errors.password ? 'true' : 'false'}
             type="password"
             placeholder="Enter your password"
             className="w-full px-4 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-blue-500"
           />
+          {errors.password?.type === 'required' && (
+            <p role="alert" className="text-red-500"> - Password is required*</p>
+          )}
         </div>
 
         <div>
           <label className="block mb-2 font-medium">Confirm Password</label>
           <input
-            {...register('confirmPassword')}
+            {...register('confirmPassword', { required: true })}
+            aria-invalid={errors.confirmPassword ? 'true' : 'false'}
             type="password"
             placeholder="Confirm your password"
             className="w-full px-4 py-2 border rounded-lg outline-none focus:ring-2 focus:ring-blue-500"
           />
+           {errors.confirmPassword?.type === 'required' && (
+            <p role="alert" className="text-red-500"> Please Re-type your password</p>
+          )}
         </div>
 
         <button
